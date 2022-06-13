4b9b86bfb957b95b773b4b31cad5974baf0f8518
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-13 21:51:22 +0300

    completing assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (namesArray, string) =>
+namesArray.filter((element) => element.toLowerCase() == string.toLowerCase());
+
+const fuzzyMatch = (namesArray, string) =>
+namesArray.filter((element)=> element.slice(0,2) == string.slice(0,2));
+
+const matchName = (namesArray, string )=>
+namesArray.filter((element) => element.name == string);
